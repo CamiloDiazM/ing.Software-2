06e9a148ea08a70710cb99ee41c07d694b20ef46
CamiloDiazM <152418160+CamiloDiazM@users.noreply.github.com>
2025-02-28 15:51:11 -0500

    Update Main.java

--- src/main/java/Model/co/edu/poli/ejemplo1/Main.java
+++ src/main/java/Model/co/edu/poli/ejemplo1/Main.java
@@ -1,9 +1,8 @@
 package Model.co.edu.poli.ejemplo1;
-
 import Model.co.edu.poli.ejemplo1.Views.ProductoView;
-import Model.co.edu.poli.ejemplo1.Views.PedidoView;
 import Model.co.edu.poli.ejemplo1.Views.ClienteView;
 
+import java.sql.SQLException;
 import java.util.Scanner;
 /*
  * Samuel Cajica Pacheco
@@ -13,16 +12,15 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         Scanner scanner = new Scanner(System.in);
         int opcion;
 
         do {
             System.out.println("Menu Principal:");
             System.out.println("1. Gestionar Productos");
-            System.out.println("2. Gestionar Pedidos");
-            System.out.println("3. Gestionar Clientes");
-            System.out.println("4. Salir");
+            System.out.println("2. Gestionar Clientes");
+            System.out.println("3. Salir");
             System.out.print("Seleccione una opcion: ");
             opcion = scanner.nextInt();
             scanner.nextLine(); // Consume newline
@@ -33,14 +31,10 @@ public class Main {
                     productoView.mostrarMenu();
                     break;
                 case 2:
-                    PedidoView pedidoView = new PedidoView();
-                    pedidoView.mostrarMenu();
-                    break;
-                case 3:
                     ClienteView clienteView = new ClienteView();
                     clienteView.mostrarMenu();
                     break;
-                case 4:
+                case 3:
                     System.out.println("Saliendo...");
                     break;
                 default:
